300b4bc903077c303c58788ed23d100555a92a78
Grigory-Dripak <126253741+Grigory-Dripak@users.noreply.github.com>
2023-09-22 02:18:50 +0300

    4-5 tasks

--- 4.js
+++ 4.js
@@ -1,9 +1,64 @@
 /* 
-Pаписать функцию, которая создает пустой объект, но без прототипа.
+Реализовать следующее консольное приложение подобно примеру, который разбирался в видео. Реализуйте его на прототипах.
+
+Определить иерархию электроприборов. Включить некоторые в розетку. Посчитать потребляемую мощность. 
+
+Таких приборов должно быть, как минимум, два (например, настольная лампа и компьютер). Выбрав прибор, подумайте, какими свойствами он обладает.
+
+План:
+
+        Определить родительскую функцию с методами, которые включают/выключают прибор из розетки.
+        Создать делегирующую связь [[Prototype]] для двух конкретных приборов.
+        У каждого из приборов должны быть собственные свойства и, желательно, методы, отличные от родительских методов.
+        Создать экземпляры каждого прибора.
+        Вывести в консоль и посмотреть результаты работы, гордиться собой. :)
+
 */
 
-function createEmpty(){
-    return Object.create(null);
+function ElectricObj(name){
+    this.name = name,
+    this.isWorking = false
 }
 
-console.log (createEmpty());
+ElectricObj.prototype.switching = function(tumbler){
+    if (tumbler=='on'){
+        this.isWorking = true
+    } else {
+        this.isWorking = false
+    }
+}
+
+function ElectricObjParameters(name, power){
+    this.name = name,
+    this.power = power
+}
+
+ElectricObjParameters.prototype = new ElectricObj();
+
+ElectricObjParameters.prototype.getPowerWorking = function(){
+    console.log(`${this.name} with ${this.power} Power is ${this.isWorking ? 'on' : 'off'}`)    
+}
+
+const pc = new ElectricObjParameters('Laptop', 50);
+const tv = new ElectricObjParameters('LCD TV', 60);
+const lamp = new ElectricObjParameters('lamp', 15);
+pc.getPowerWorking();
+tv.getPowerWorking();
+lamp.getPowerWorking();
+pc.switching('on');
+tv.switching('on');
+lamp.switching('on');
+pc.getPowerWorking();
+tv.getPowerWorking();
+lamp.getPowerWorking();
+lamp.switching('off');
+
+function totalPowerCalc(objArray){
+    let totalPower = 0
+    for (let i of objArray){
+        i.isWorking ? totalPower += i.power : totalPower += 0
+    }
+    return totalPower
+}
+
+console.log(totalPowerCalc([pc, tv, lamp]))
--- 5.js
+++ 5.js
@@ -1,9 +1,55 @@
 /* 
-Pаписать функцию, которая создает пустой объект, но без прототипа.
+Переписать консольное приложение из предыдущего юнита на классы.
+
 */
 
-function createEmpty(){
-    return Object.create(null);
+
+class ElectricObj{
+    constructor(name){
+        this.name = name;
+        this.isWorking = false
+    }
+
+    switching(tumbler){
+        if (tumbler==='on'){
+            this.isWorking = true
+        } else {
+            this.isWorking = false
+        }
+    }
 }
 
-console.log (createEmpty());
+class ElectricObjParameters extends ElectricObj{
+    constructor(name, power){
+        super(name);
+        this.power = power
+    }
+
+    getPowerWorking(){
+        console.log(`${this.name} with ${this.power} Power is ${this.isWorking ? 'on' : 'off'}`)    
+    }
+}
+
+const pc = new ElectricObjParameters('Laptop', 50);
+const tv = new ElectricObjParameters('LCD TV', 60);
+const lamp = new ElectricObjParameters('lamp', 15);
+pc.getPowerWorking();
+tv.getPowerWorking();
+lamp.getPowerWorking();
+pc.switching('on');
+tv.switching('on');
+lamp.switching('on');
+pc.getPowerWorking();
+tv.getPowerWorking();
+lamp.getPowerWorking();
+lamp.switching('off');
+
+function totalPowerCalc(objArray){
+    let totalPower = 0
+    for (let i of objArray){
+        i.isWorking ? totalPower += i.power : totalPower += 0
+    }
+    return totalPower
+}
+
+console.log(totalPowerCalc([pc, tv, lamp]))
